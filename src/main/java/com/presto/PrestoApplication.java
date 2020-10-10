0dee72fd17d116880c1034b476f6183fd443ba71
unknown <fastcaf@hotmail.com>
2020-10-10 12:39:12 -0300

    Inserção de controller, service e repositorio para upload de imagem

--- src/main/java/com/presto/PrestoApplication.java
+++ src/main/java/com/presto/PrestoApplication.java
@@ -17,5 +17,4 @@ public class PrestoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(PrestoApplication.class, args);
 	}
-
 }
---
+++ src/main/java/com/presto/service/ImageServiceImp.java
@@ -0,0 +1,60 @@
+package com.presto.service;
+
+import com.presto.repository.ImageRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.util.zip.DataFormatException;
+import java.util.zip.Deflater;
+import java.util.zip.Inflater;
+
+@Service
+public class ImageServiceImp implements ImageService{
+
+    @Autowired
+    ImageRepository imageRepository;
+
+    @Override
+    public byte[] compressBytes(byte[] data) {
+        Deflater deflater = new Deflater();
+        deflater.setInput(data);
+        deflater.finish();
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream(data.length);
+        byte[] buffer = new byte[1024];
+        while (!deflater.finished()) {
+            int count = deflater.deflate(buffer);
+            outputStream.write(buffer, 0, count);
+        }
+        try {
+            outputStream.close();
+        } catch (IOException e) {
+
+        }
+        System.out.println("Compressed Image Byte Size - " + outputStream.toByteArray().length);
+        return outputStream.toByteArray();
+    }
+
+    @Override
+    public byte[] decompressBytes(byte[] data) {
+        Inflater inflater = new Inflater();
+        inflater.setInput(data);
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream(data.length);
+        byte[] buffer = new byte[1024];
+        try {
+            while (!inflater.finished()) {
+                int count = inflater.inflate(buffer);
+                outputStream.write(buffer, 0, count);
+
+            }
+            outputStream.close();
+        } catch (IOException ioe) {
+        } catch (DataFormatException e) {
+
+        }
+        return outputStream.toByteArray();
+    }
+}
